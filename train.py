38ab93efa6e2b0036b9e71c78cdedf300fe74f34
jggrobelny <113335584+jggrobelny@users.noreply.github.com>
2022-09-22 17:36:50 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,20 +1,19 @@
 import os
 from PIL import Image
-import numpy as np    
+import numpy as np
 import cv2
 import pickle
 
-BASE_DIR=os.path.dirname(os.path.abspath(__file__))  #lokalizacja skryptu
-image_dir = os.path.join(BASE_DIR, "images")          #folder z twarzami
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # lokalizacja skryptu
+image_dir = os.path.join(BASE_DIR, "images")  # folder z twarzami
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 
-current_id=0
-label_ids  = {}
+current_id = 0
+label_ids = {}
 x_train = []
 y_labels = []
-
 
 for root, dirs, files in os.walk(image_dir):
     for file in files:
@@ -27,21 +26,22 @@ for root, dirs, files in os.walk(image_dir):
                 current_id += 1
             id_ = label_ids[label]
             print(label_ids)
-            pil_image = Image.open(path).convert("L") #skala szaro
-            image_array = np.array(pil_image, "uint8")
+            pil_image = Image.open(path).convert("L")  # skala szaro
+            size = (550, 550)
+            final_image = pil_image.resize(size, Image.ANTIALIAS)
+            image_array = np.array(final_image, "uint8")
             faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=3)
 
-            for(x,y,w,h) in faces:
-                roi = image_array[y:y+h, x:x+w]
+            for (x, y, w, h) in faces:
+                roi = image_array[y:y + h, x:x + w]
                 x_train.append(roi)
                 y_labels.append(id_)
 
+# print(y_labels)
+# print(x_train)
 
-#print(y_labels)
-#print(x_train)
+with open("labels.pickle", 'wb') as f:
+    pickle.dump(label_ids, f)
 
-with open("labels.pickle",'wb') as f:
-    pickle.dump(label_ids,f)
-
-recognizer.train(x_train,np.array(y_labels))
+recognizer.train(x_train, np.array(y_labels))
 recognizer.save("trainer.yml")
